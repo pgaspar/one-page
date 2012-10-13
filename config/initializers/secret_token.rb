1cb9629cad3010894242c45ba8bf545e9b7fcfd6
Pedro Gaspar <pedro.gaxpar@gmail.com>
2012-10-13 17:01:24 +0100

    Rails init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsrumble::Application.config.secret_token = '05dc7f113d7514b092f199da5f33c770fd35190966a07252bf0d10dedafb1f1ca7f666ebb6794d4a5b701e548aaa0bbd4d661b8774a5b4d5b8136668edd9c2ea'
