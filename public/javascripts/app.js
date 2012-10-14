5c471fef6826a6714b88caf68e1f08706d6cf915
Pedro Gaspar <pedro.gaxpar@gmail.com>
2012-10-14 01:58:18 +0100

    JS for creating new sections

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -18,10 +18,11 @@ $(document).ready(function() {
 	    buttons: ''
 	});
 
-	$('.bs-docs-sidenav').affix({
-      offset: {
-        top: function () { return $(window).width() <= 980 ? 290 : 210 }
-      , bottom: 270
-      }
-    })
+  $('.add-section').on('click', function(){
+    var nav_code     = "<li><a href=\"#section-" + $("section").size() + "\"><i class=\"icon-chevron-right\"></i> New Section</a></li>";
+    var section_code = "<section id=\"section-" + $("section").size() + "\"><div class=\"page-header\"><h1>New Section</h1></div><p class=\"editable\">Write your content here.</p></section>";
+
+    $(".nav-list li:not(.add-section)").last().after(nav_code);
+    $("section").last().after(section_code);
+  });
 });
